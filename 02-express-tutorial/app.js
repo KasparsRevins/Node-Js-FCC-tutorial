f33dbb13886d683ab3af05ef96c39ff34d9fa06a
Kaspars Revins <okoikwedced@gmail.com>
2023-12-07 12:36:54 +0000

    Express vs Inbuilt http module

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,23 +1,27 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-const homePage = readFileSync("./index.html");
+app.get("/",(req, res)=>{
+  console.log("User hit the resource");
+  res.status(200).send("Home Page")
+})
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page</h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>page not found</h1>");
-    res.end();
-  }
-});
+app.get("/about",(req, res)=>{
+  res.status(200).send("About Page")
+})
 
-server.listen(5000);
+app.all("*",(req, res)=>{
+  res.status(404).send("<h1>Resource not found</h1>")
+})
+
+app.listen(5000,()=>{
+  console.log("Server is listening on port 5000...")
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
