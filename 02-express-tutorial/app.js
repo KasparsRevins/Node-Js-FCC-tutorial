e74266ec5f5ef719a716b27a235a86601a9f7c1f
Kaspars Revins <okoikwedced@gmail.com>
2023-12-08 17:12:50 +0000

    Express static files

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,18 @@
-const express = require("express");
+const express = require('express');
+const path = require("path");
+
 const app = express();
 
-app.get("/",(req, res)=>{
-  console.log("User hit the resource");
-  res.status(200).send("Home Page")
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"./navbar-app/index.html"))
 })
 
-app.get("/about",(req, res)=>{
-  res.status(200).send("About Page")
-})
-
-app.all("*",(req, res)=>{
-  res.status(404).send("<h1>Resource not found</h1>")
+app.all('*',(req,res)=>{
+  res.status(404).send("Resource not found")
 })
 
 app.listen(5000,()=>{
-  console.log("Server is listening on port 5000...")
+  console.log("Server is listening on port 5000...");
 })
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
