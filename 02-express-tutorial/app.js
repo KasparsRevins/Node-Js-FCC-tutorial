46b1d56159a6cf75755e7fee55c411684a207e4b
Kaspars Revins <okoikwedced@gmail.com>
2023-12-09 16:46:54 +0000

    Route params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const path = require("path");
-
+const express = require("express");
 const app = express();
+const {products} = require("./data")
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,"./navbar-app/index.html"))
+app.get("/", (req,res) => {
+  res.send("<h1> Home Page </h1><a href='/api/products'>Products</a>")
+});
+app.get("/api/products",(req,res)=>{
+  const newProducts = products.map((product)=>{
+    const {id,name,image} = product;
+    return {id,name,image}
+  })
+  res.json(newProducts)
 })
-
-app.all('*',(req,res)=>{
-  res.status(404).send("Resource not found")
+app.get("/api/products/:productID",(req,res)=>{
+  // console.log(req);
+  // console.log(req.params);
+  const {productID} = req.params;
+  const singleProduct = products.find((product)=> product.id === Number(productID)) 
+  if(!singleProduct){
+    return res.status(404).send("Product does not exist")
+  }
+  return res.json(singleProduct)
 })
-
-app.listen(5000,()=>{
+app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
-})
+});
