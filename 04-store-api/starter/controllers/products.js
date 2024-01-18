d1d0d16c7442af7b096644576a3cdd7d73fce8f3
Kaspars <okoikwedced@gmail.com>
2024-01-18 13:20:59 +0000

    Mongoose numeric filter

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,12 +2,12 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
     const search = 'ab'
-    const products = await Product.find({}).sort('name').select('name price')
+    const products = await Product.find({ price: { $gt: 30 } }).sort('price').select('name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -22,7 +22,26 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
-    // console.log(queryObject);
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-')
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        })
+    }
+
+    console.log(queryObject);
     let result = Product.find(queryObject)
     // sort
     if (sort) {
